c8f9daac8590ca182e2a5f650346ae2c05b6c399
Eder Soares <edersoares@me.com>
2018-05-02 14:03:49 -0300

    Added more integration tests for Irrf calculator

--- tests/Integration/IrrfCalculatorIntegrationTest.php
+++ tests/Integration/IrrfCalculatorIntegrationTest.php
@@ -21,4 +21,79 @@ class IrrfCalculatorIntegrationTest extends TestCase
         $this->assertEquals(4806, $irrf->getBaseValue());
         $this->assertEquals(27.5, $irrf->getAliquot());
     }
+
+    public function testSalaryUntilOneThousand()
+    {
+        $irrfCalculator = new IrrfCalculator(
+            new InssCalculator()
+        );
+
+        $irrf = $irrfCalculator->calculate(
+            new Salary(1000)
+        );
+
+        $this->assertEquals(0, $irrf->getValue());
+        $this->assertEquals(920, $irrf->getBaseValue());
+        $this->assertEquals(0, $irrf->getAliquot());
+    }
+
+    public function testSalaryUntilThreeThousand()
+    {
+        $irrfCalculator = new IrrfCalculator(
+            new InssCalculator()
+        );
+
+        $irrf = $irrfCalculator->calculate(
+            new Salary(3000)
+        );
+
+        $this->assertEquals(57.45, $irrf->getValue());
+        $this->assertEquals(2670, $irrf->getBaseValue());
+        $this->assertEquals(7.5, $irrf->getAliquot());
+    }
+
+    public function testSalaryUntilFourThousand()
+    {
+        $irrfCalculator = new IrrfCalculator(
+            new InssCalculator()
+        );
+
+        $irrf = $irrfCalculator->calculate(
+            new Salary(4000)
+        );
+
+        $this->assertEquals(179.2, $irrf->getValue());
+        $this->assertEquals(3560, $irrf->getBaseValue());
+        $this->assertEquals(15, $irrf->getAliquot());
+    }
+
+    public function testSalaryUntilFiveThousand()
+    {
+        $irrfCalculator = new IrrfCalculator(
+            new InssCalculator()
+        );
+
+        $irrf = $irrfCalculator->calculate(
+            new Salary(5000)
+        );
+
+        $this->assertEquals(365.12, $irrf->getValue());
+        $this->assertEquals(4450, $irrf->getBaseValue());
+        $this->assertEquals(22.5, $irrf->getAliquot());
+    }
+
+    public function testSalaryUntilSixThousand()
+    {
+        $irrfCalculator = new IrrfCalculator(
+            new InssCalculator()
+        );
+
+        $irrf = $irrfCalculator->calculate(
+            new Salary(6000)
+        );
+
+        $this->assertEquals(609.85, $irrf->getValue());
+        $this->assertEquals(5378.96, $irrf->getBaseValue());
+        $this->assertEquals(27.5, $irrf->getAliquot());
+    }
 }
